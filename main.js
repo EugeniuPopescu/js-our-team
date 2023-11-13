648d7b1a6a0c68d3351616bd20cb4442460f67a7
Eugen <epopescuu@yahoo.it>
2023-11-13 11:21:02 +0100

    feat: add box in html

--- main.js
+++ main.js
@@ -1,36 +1,42 @@
+'use strict';
+
 // array di oggetti
 let team = [
-    membro1 = {
+    {
         nome: 'Wayne Barnett',
         ruolo: 'Founder & CEO',
         foto: 'wayne-barnett-founder-ceo.jpg'
     },
-    membro2 = {
+    {
         nome: 'Angela Caroll',
         ruolo: 'Chief Editor',
         foto: 'angela-caroll-chief-editor.jpg'
     },
-    membro3 = {
+    {
         nome: 'Walter Gordon',
         ruolo: 'Office Manager',
         foto: 'walter-gordon-office-manager.jpg'
     },
-    memebro4 = {
+    {
         nome: 'Angela Lopez',
         ruolo: 'Social Media Manager',
         foto: 'angela-lopez-social-media-manager.jpg'
     },
-    memebro5 = {
+    {
         nome: 'Scott Estrada',
         ruolo: 'Developer',
         foto: 'scott-estrada-developer.jpg'
     },
-    membro6 = {
+    {
         nome: 'Barbara Ramos',
         ruolo: 'Graphic Designer',
         foto: 'barbara-ramos-graphic-designer.jpg'
     }
 ]
+
+// mi prendo il container 
+let container = document.querySelector('.box-container');
+
 
 // stampo l'array con un ciclo for
 for (let i = 0; i < team.length; i++) {
@@ -38,12 +44,23 @@ for (let i = 0; i < team.length; i++) {
     // stampo gli oggetti
     // console.log(individuo);
     
+    let box = `<div class="box bg-primary text-dark my-5">
+        <img src="./img/${individuo.foto}">
+        <p class="nome text-center fs-3">${individuo.nome}</p>
+        <p class="ruolo text-center fs-4">${individuo.ruolo}</p>
+    </div>`
+
+    container.innerHTML += box;
+    
+    
     // stampo con for in
     for (const key in team[i]) {
         console.log(`memebro-${i}`, key, individuo[key]);
-        document.querySelector('#team').innerHTML += `${key}: ${individuo[key]}<br>`;
+        // document.querySelector('#team').innerHTML += `${key}: ${individuo[key]}<br>`;
+        
+        // document.createElement('div').innerHTML
+        // document.classList.add('');
         
     }
     console.log('--------------------------------');
-    document.querySelector('#team').innerHTML += `-------------------------------- <br>`;
 }
